1e519bbe49f5b0528e30e51130e822e742291737
Egeberk Özberk <egeberk@gmail.com>
2022-06-08 22:01:49 +0300

    Collection view template and proto custom cell design are added. Both will be modified later.

--- CryptoHW/ViewController.swift
+++ CryptoHW/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // UI Elements
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Collection View
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        // Register custom cell
+        collectionView.register(.init(nibName: "CustomCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CustomCollectionViewCell")
+        
+        
     }
 
 
+}
+
+extension ViewController:UICollectionViewDelegate{
+    
+}
+
+extension ViewController:UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Cell definition
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as! CustomCollectionViewCell
+        
+        // Cell data
+        cell.nameLabel.text = "Test " + "\(indexPath.row)"
+        
+        return cell
+    }
 }
 
